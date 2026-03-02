54cf8614f1ca36e6b7b63083eee78acb88ffc2cb
frnandxr <frnandxr@gmail.com>
2026-03-01 21:25:40 -0600

    Día 1 finalizado: Clase Material blindada y gestión de listas completada

--- models.py
+++ models.py
@@ -9,40 +9,43 @@ class Material :
     def validacion_nombre(self, nombre):
         if not isinstance (nombre , str):
             raise ValueError("Ingresa un nombre correcto")
-        return (material)
+        return (nombre)
     def validacion_precio(self, precio):
         if not isinstance(precio, float ) or (precio) < 0:
             raise ValueError ("Ingresa un numero entero valido")
-        return (costo)
+        return (precio)
     def validacion_prov(self, provedor):
         if not isinstance(provedor, str):
             raise ValueError("Ingresa un nombre de provedor valido")
-        return(prov)
+        return(provedor)
     def validacion_stock(self, stock):
         if not isinstance(stock, int ) or (stock) < 0:
             raise ValueError ("Ingresa un numero de stock valido")
-        return (stk)
+        return (stock)
     #Una fucnion para que se repita con todos los materiales y se imprima el objeto definido y sus atributos
     def __str__(self):
         return f'Material:{self.nombre}| Precio:${self.precio}| Provedor :{self.provedor}'
 list_mat = []
 while True:
     try:
-        material = str(input('Escribe el nombre del material: '))
+        nombre = str(input('Escribe el nombre del material: '))
         #se pregunta el costo 
-        costo = float(input("Ingresa el precio del material: "))
+        precio = float(input("Ingresa el precio del material: "))
         #provedor
-        prov = str(input('Nombre del proveedor: '))
+        provedor = str(input('Nombre del proveedor: '))
         #se define mat, es la que junta todo (clase,objeto,atributos)
-        stk = int(input(f'Ingresa la cantidad del {material} en stock: '))
-        mat = Material(material, costo, stk, prov)
+        stock = int(input(f'Ingresa la cantidad del {nombre} en stock: '))
+        mat = Material(nombre,precio,provedor,stock)
         print(mat)
         list_mat.append(mat)
-        print("lista actualizadac exitosamente:",list_mat)
         respuesta= input("¿Deseas agregar un nuevo material o salir?")
         if respuesta.lower() == 'salir':  
             break
     except ValueError:
-        print("Ingresa los datos correctamente")
-
+        print(f'error {nombre} ')
+        print(f'error {precio}')
+        print(f'error {provedor}')
+        print(f'error {stock}')
+for material in list_mat:
+    print(f'lista actualizada con exito:{material}')
 
