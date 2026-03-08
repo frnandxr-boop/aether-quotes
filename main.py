36edb84b70bdcb81ab5cb213aad62d3e00fbf585
frnandxr <frnandxr@gmail.com>
2026-03-08 02:56:54 -0600

    Semana 1 finalizada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from models import Material
 import json
+ 
 #trata de:
 try: 
     #Ver y/o leer el archivo existente antes de agregar mas datos y en caso de estar vacio o no existir se creara uno vacio
@@ -10,41 +11,103 @@ except FileNotFoundError:
 
 datos_objetos = [Material.from_inventario(d) for d in datos_guardados]
 total_dinero = 0
+total_truper = 0
 for m in datos_objetos:
     total_dinero += m.precio * m.stock
-
-print(f"--- Sistema Iniciado: Valor Total en Almacén: ${total_dinero} ---")
+    if m.proveedor.lower() == 'truper':
+        total_truper += m.precio * m.stock
+print(f"--- Sistema Iniciado: Valor Total en Almacén: ${total_dinero} , valor de truper : ${total_truper} ---")
 #Nuestra lista vacia 
 list_mat = []
 #Se inicia un bucle while para que se puedan agregar mas datos a nuestra lista
 while True:
+    
     try:
-        #se le pregunta al usuario el nombre del material 
-        nombre = str(input('Escribe el nombre del material: '))
-        #se pregunta el costo 
-        precio = float(input(f"Ingresa el precio de {nombre}: "))
-        #provedor
-        provedor = str(input('Nombre del proveedor: '))
-        #se define mat, es la que junta todo (clase,objeto,atributos)
-        stock = int(input(f'Ingresa la cantidad de {nombre} en stock: '))
-        mat = Material(nombre,precio,provedor,stock)
-        print(mat)
-        list_mat.append(mat)
-        #Se pregunta si desea continuar o salir 
-        respuesta= input("¿Deseas agregar un nuevo material o salir? ")
-        #si la respuesta es 'salir' entonces el bucle acaba 
-        if respuesta.lower() == 'salir':  
+        #Se le muestra al usuario las acciones que puede realizar
+        print("Acciones : Agregar material | Editar valores | Eliminar material | Buscar Material | Salir ")
+        #Se crea la variable 'accion' para guardar la respuesta del usuario
+        accion = input("¿Que accion deseas realizar?: ")
+        #Para buscar un material la respues guardada en "accion" debe ser "buscar material"
+        if  accion.lower() == 'buscar material':
+            print("Buscador")
+            material_buscado = []
+            encontrado = False
+            busqueda=input("¿Que material buscas?")
+            for material in datos_objetos :
+                if material.nombre.lower() == busqueda:
+                    print(material)
+                    encontrado = True
+                    break 
+            if not encontrado:
+                print(f'{busqueda} no encontrado')
+        elif accion.lower()  == 'agregar material':
+            #se le pregunta al usuario el nombre del material 
+            nombre = str(input('Escribe el nombre del material: '))
+            #se pregunta el costo 
+            precio = float(input(f"Ingresa el precio de {nombre}: "))
+            #provedor
+            proveedor = str(input('Nombre del proveedor: '))
+            #se define mat, es la que junta todo (clase,objeto,atributos)
+            stock = int(input(f'Ingresa la cantidad de {nombre} en stock: '))
+            mat = Material(nombre,precio,proveedor,stock)
+            print(mat)
+            list_mat.append(mat)
+            
+        #Para editar los materiales en el json ("editar material")  
+        elif accion.lower()  == 'editar material':
+            nvo_precio =[]
+            nvo_stock=[]
+            nvo_proveedor=[]
+            todo=[]
+            material_a_editar= input("Escribe el material a editar: ")
+            
+            material_edit=next((m for m in datos_objetos if m.nombre == material_a_editar), None)
+            edit=input("¿Que deseas editar(stock,proveedor,precio,todo)? ")
+            #funciones para todas los posibles cambios/ediciones
+            def editar_stock(material):
+                    nvo_stock = int(input(f"Ingresa la cantidad en stock de {material.nombre}: "))
+                    material_edit.stock = nvo_stock
+                    print(f'stock actualizado con exito: {material.nombre} -- {material.stock}')
+                    
+            def editar_precio(material):    
+                    nvo_precio = float(input(f"Ingresa el nuevo precio de {material.nombre}: "))
+                    material_edit.precio= nvo_precio
+                    print(f'Precio actualizado con exito: {material.nombre} -- {material.precio}')
+                    
+            def editar_proveedor(material):       
+                    nvo_proveedor = str(input(f'ingresa el nuevo proveedor de {material.nombre}: '))
+                    material_edit.proveedor = nvo_proveedor
+                    print(f'Provedor actualizado con exito: {material.nombre} -- {material.proveedor}')
+            #Se crea una funcion que englobe todo 
+            def editar_todo(material):
+                    editar_stock(material_edit)
+                    editar_precio(material_edit)
+                    editar_proveedor(material_edit)
+                    print(f"{material.nombre} editado con exito : {material.nombre}-- {material.precio}--{material.proveedor}--{material.stock}")
+            #Se llaman y activan las funciones 
+            if edit.lower() == 'stock':
+                editar_stock(material_edit)
+            elif edit.lower() == 'precio':
+                editar_precio(material_edit)
+            elif edit.lower() == 'proveedor':
+                editar_proveedor(material_edit)
+            elif edit.lower() == 'todo':
+                editar_todo(material_edit)
+            #Aqui terminan las acciones y se pregunta si quiere salir o realizar una nueva accion
+            
+        elif accion.lower() == "salir":
             break
     #si un dato esta mal, salta error y se apoda 'e' asi sabemos exactamente cual fue el error
     except ValueError as e:
         print(f'error {e} ')
 #se crea el diccionario recorriendo la lista con todos sus datos 
 #__dict__ se utiliza para 'transformar' los datos de la lista en texto que el json pueda leer
-dic_mat_list= [material.to_dict() for material in list_mat]
+####dic_mat_list= [material.to_dict() for material in datos_objetos + list_mat]###
+datos_g=[material.to_dict() for material in datos_objetos + list_mat]
 #-----Creacion de .JSON----
 #Se define una lista a la cual se agregaran los nuevos datos
-lista_final= datos_guardados + dic_mat_list
+###lista_final= datos_guardados + dic_mat_list##
 #se abre el archivo .JSON y se escriben los nuevos materiales agregando los que ya estaban anteriormente
 with open ('inventario.json', 'w') as mat:
-   json.dump(lista_final, mat, indent=2)
+   json.dump(datos_g, mat, indent=2)
 lista_mat_final =[]
--- models.py
+++ models.py
@@ -2,10 +2,10 @@ import json
 #Creamos la clase llamada "Material"
 class Material :
     #se llama al "contructor" (__init__) con el objetio (self) y los atributos  con las condiciones (str para texto y float para numeros)
-    def __init__(self, nombre : str, precio: float , provedor : str , stock :int ):
+    def __init__(self, nombre : str, precio: float , proveedor : str , stock :int ):
         self.nombre = self.validacion_nombre(nombre)
         self.precio = self.validacion_precio(precio)
-        self.provedor = self.validacion_prov(provedor)
+        self.proveedor = self.validacion_prov(proveedor)
         self.stock = self.validacion_stock(stock)
     def validacion_nombre(self, nombre):
         if not isinstance (nombre , str) or not nombre.strip():
@@ -15,26 +15,26 @@ class Material :
         if not isinstance(precio, (float, int) ) or (precio) < 0:
             raise ValueError ("Ingresa un numero valido")
         return (precio)
-    def validacion_prov(self, provedor):
-        if not isinstance(provedor, str) or not provedor.strip():
+    def validacion_prov(self, proveedor):
+        if not isinstance(proveedor, str) or not proveedor.strip():
             raise ValueError("Ingresa un nombre de provedor valido")
-        return(provedor)
+        return(proveedor)
     def validacion_stock(self, stock):
-        if not isinstance(stock, (int, int) ) or (stock) < 0:
+        if not isinstance(stock, int)  or (stock) < 0:
             raise ValueError ("Ingresa un numero de stock valido")
         return (stock)
     def to_dict (self):
         return {
             'nombre' : self.nombre,
             'precio': self.precio,
-            'provedor' : self.provedor,
+            'proveedor' : self.proveedor,
             'stock' : self.stock
         }
     @classmethod
     def from_inventario (cls, datos):
-        return cls(nombre=datos['nombre'], precio=datos['precio'], provedor=datos['provedor'], stock=datos['stock'])
+        return cls(nombre=datos['nombre'], precio=datos['precio'], proveedor=datos['proveedor'], stock=datos['stock'])
     def __repr__(self):
-        return f"Material(nombre='{self.nombre}', precio='{self.precio}',provedor'{self.provedor}', stock{self.stock})"
+        return f"Material(nombre='{self.nombre}', precio='{self.precio}',proveedor='{self.proveedor}', stock={self.stock})"
     #Una fucnion para que se repita con todos los materiales y se imprima el objeto definido y sus atributos
     def __str__(self):
-        return f'Material:{self.nombre}| Precio:${self.precio}| Provedor :{self.provedor}| Stock:{self.stock}'
+        return f'Material:{self.nombre}| Precio:${self.precio}| Proveedor :{self.proveedor}| Stock:{self.stock}'
